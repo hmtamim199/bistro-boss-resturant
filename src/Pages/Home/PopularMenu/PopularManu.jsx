19fbdc921a5411cc310ac8a9576dacc2b2a1d789
hmtamim199 <hmtamim8906@gmail.com>
2024-03-06 07:52:41 +0600

    custom hooks started

--- src/Pages/Home/PopularMenu/PopularManu.jsx
+++ src/Pages/Home/PopularMenu/PopularManu.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import Menuitem from "../../Shared/Menu-item/Menuitem";
+import useMenu from "../../../Hooks/UseMenu";
 
 const PopularManu = () => {
-  const [menu, setMenu] = useState([])
-  useEffect(() => {
-    fetch('menu.json')
-      .then(res => res.json())
-      .then(data => {
-        const popularItems = data.filter(item => item.category === "popular")
-        setMenu(popularItems)
-      })
-  }, [])
+  const [menu] = useMenu();
+  const popular = menu.filter(item => item.category === "popular");
+  // const [menu, setMenu] = useState([])
+
+  // useEffect(() => {
+  //   fetch('menu.json')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       const popularItems = data.filter(item => item.category === "popular")
+  //       setMenu(popularItems)
+  //     })
+  // }, [])
 
 
   return (
@@ -23,7 +27,7 @@ const PopularManu = () => {
 
       <div className="grid md:grid-cols-2 gap-4">
         {
-          menu.map(item => <Menuitem
+          popular.map(item => <Menuitem
             key={item._id}
             item={item}
           ></Menuitem>)
